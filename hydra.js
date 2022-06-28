b60d4c47338f1d6eb43fcbfbf378edc44bf5ec6a
Alexander Gilbertson <alexander.gilbertson@icloud.com>
2022-06-28 12:43:40 -0500

    Update hydra.js

--- hydra.js
+++ hydra.js
@@ -1,6 +1,14 @@
-// Idea 11
+// Idea 1
 
 osc(8.345, 0.153, 1.143)
 	.kaleid(4.408)
 	.scale(3.5)
 	.out();
+
+// Idea 2
+noise().mult(osc(8.345, 0.153, 1.143)
+	.kaleid(9.408)
+	.scale(0.5))
+	.out();
+
+
