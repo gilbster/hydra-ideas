eaca01b81c5aa5ffe033e46f475623d3803bb1b4
Alexander Gilbertson <alexander.gilbertson@icloud.com>
2022-06-28 12:58:09 -0500

    Update hydra.js

--- hydra.js
+++ hydra.js
@@ -11,4 +11,24 @@ noise().mult(osc(8.345, 0.153, 1.143)
 	.scale(0.5))
 	.out();
 
+// Idea 3
+osc(10.281, 0.144, 1.2)
+	.blend(noise(1.272)).color(-2, 1).colorama()
+	.diff(shape(1, 0.02)
+		.rotate(0.834, 0.067))
+	.out();
 
+// Now Combine 
+osc(10.281, 0.059, 1.2)
+	.blend(noise(1.376, 0)
+		.thresh(0, 156)
+		.add(noise(21.33, 0)
+			.brightness(-1)
+			.color(0, 0.551)), -.003)
+	.color(-1.641, 1.862)
+	.colorama()
+	.blend(noise(1.272))
+	.color(-2, 1)
+	.colorama()
+	.diff(noise(1.376, 0))
+	.out();
